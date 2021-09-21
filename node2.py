181c4dbb1918f11362b4368efef6c52437fb1857
Raymond Wei <raymondwei@outlook.com>
2021-09-21 10:42:03 -0500

    2021-09-21->0.262 - progress in node2

--- node2.py
+++ node2.py
@@ -41,38 +41,42 @@ class Node2:
         return maxbit
 
     def spawn(self):
-        mbit = self.get_split_bit()
-        if not mbit:
+        sbit = self.get_split_bit()
+        self.spltbit = sbit
+        if not self.spltbit:
             return None
         vkm0 = VK12Manager()  # subset of vks, for when mbit set to 0
-        sat0 = {mbit: 0}
+        sat0 = {sbit: 0}
         vkm1 = VK12Manager()  # subset of vks, for when mbit set to 1
-        sat1 = {mbit: 1}
+        sat1 = {sbit: 1}
 
-        drp_kns = set(self.vkm.bdic.pop(mbit))
+        # drp_kns = set(self.vkm.bdic.pop(sbit))
+        drp_kns = set(self.vkm.bdic[sbit])
         kns = sorted(self.vkm.kn2s[:])  # all kns of vk2s in self.vkm
         for kn in drp_kns:
             kns.remove(kn)      # drop-out kn
             vk = self.vkm.vkdic.pop(kn)
-            if vk.dic[mbit] == 0:
-                vkm0.add_vk1(vk.drop_bit(mbit))
+            if vk.dic[sbit] == 0:
+                vkm0.add_vk1(vk.drop_bit(sbit))
             else:
-                vkm1.add_vk1(vk.drop_bit(mbit))
+                vkm1.add_vk1(vk.drop_bit(sbit))
         for kn in kns:
             vk2 = self.vkm.vkdic.pop(kn)
             vkm0.add_vk2(vk2.clone())  # add_vk2 may modify vk2, clone, so
             vkm1.add_vk2(vk2)          # vkm1/add_vk2 won't have it wrong
         sd0 = set(self.vkm.bdic) - set(vkm0.bdic)
         for b in sd0:
-            sat0[b] = 2
+            if b != sbit:
+                sat0[b] = 2
         sd1 = set(self.vkm.bdic) - set(vkm1.bdic)
         for b in sd1:
-            sat1[b] = 2
+            if b != sbit:
+                sat1[b] = 2
         node0 = Node2(vkm0, self)
         node0.add_sat(sat0)
         node1 = Node2(vkm1, self)
         node1.add_sat(sat1)
-        self.chs = [node0, node1]
+        self.chs = node0, node1  # tuple of 2 children
         for n2 in self.chs:
             n2.spawn()
 
